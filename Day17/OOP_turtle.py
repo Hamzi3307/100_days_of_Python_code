83ce8cff16936caa8fccf00d10dff162e15da0fa
Hamza Khan <hamzi3307@gmail.com>
2023-09-19 22:30:13 +0500

    Day 17 OOP Basics

---
+++ Day17/OOP_turtle.py
@@ -0,0 +1,13 @@
+from turtle import Screen, Turtle
+
+timmy = Turtle()
+
+print(timmy)
+timmy.shape("turtle")
+timmy.color("red")
+timmy.fillcolor("yellow")
+timmy.forward(100)
+
+my_scr = Screen()
+print(my_scr.canvheight)
+my_scr.exitonclick()
