d568814fe8eb900f2db2529d78d60aaf01b61724
Hamza Khan <hamzi3307@gmail.com>
2023-09-29 22:38:14 +0500

    Day 26 ComprehensionCompleted

--- Day26/phoenetic-alphabet-NATO/main.py
+++ Day26/phoenetic-alphabet-NATO/main.py
@@ -2,12 +2,12 @@ student_dict = {
     "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
 }
+e = []
 
 #Looping through dictionaries:
 for (key, value) in student_dict.items():
-    #Access key and value
     pass
-
+    
 import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 
@@ -23,5 +23,14 @@ for (index, row) in student_data_frame.iterrows():
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+nato_data_frame = pandas.read_csv("Day26\phoenetic-alphabet-NATO\phonetic_alphabet_nato.csv")
 
+dict_nato = {row.letter:row.code for (index, row) in nato_data_frame.iterrows() }
+print(dict_nato)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("What you want to say about?")
+
+list_user_input = [dict_nato[letter.upper()] for letter in list(user_input)]
+
+print(list_user_input)
